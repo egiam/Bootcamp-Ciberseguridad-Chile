7f5772a57e86af719c7fdf14c91ba5712d48cb36
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2022-10-21 18:53:19 -0700

    Update Clase 1 Parte 2

--- 1 RedTeam/C/hello.c
+++ 1 RedTeam/C/hello.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+int square(int x) {
+    return x * x;
+}
+
 int main(void)
 {
-    printf("Hello, world!"); // Print "Hello, world!"
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Hello, world!\n"); // Print "Hello, world!"
+    if (square(num) >= 25){
+        printf("Ingrese al flujo 1\n");
+    }
+    else {
+        printf("Ingrese al flujo 2\n");
+    }
+    return 0;
 }
 
 // 
